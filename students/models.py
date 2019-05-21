69b9adf01c808634f8d4b2352a243d9cc1e9fc08
Diego Resende <wdresende@gmail.com>
2019-05-20 22:26:12 -0300

    feat(#1): created class Student in model
    co-authored-by: Victor Gomide <victoragomide@gmail.com>

--- students/models.py
+++ students/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model) {
+    nome = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=10)
+    email = models.EmailField(max_length=254)
+    senha = models.CharField(max_length=15)
+}
