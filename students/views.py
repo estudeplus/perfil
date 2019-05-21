4050a6df1a26c46405149d7b245be6c959d474b1
Diego Resende <wdresende@gmail.com>
2019-05-20 23:39:16 -0300

    feat(#1): method in view for create a student
    Co-authored-by: Victor Gomide <victoragomide@gmail.com>

--- students/views.py
+++ students/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 from .models import Student
 
+def create_student(resquest):
+    form = StudentForm(resquest.POST or None)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'student-form.html', {'form': form})
