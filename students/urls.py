47ff352e30e143cbd7462e7ca5314f088c471729
Diego Resende <wdresende@gmail.com>
2019-05-21 23:36:19 -0300

    feat(#3): created url for delete a student

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_student, list_students, update_student
+from .views import list_students, create_student, update_student, delete_student
 
 urlpatterns = [
     path('', list_students, name='list_students'),
     path('new/', create_student, name='create_students'),
-    path('update/<int:id>/', update_student, name='update_student')
+    path('update/<int:id>/', update_student, name='update_student'),
+    path('delete/<int:id>/', delete_student, name='delete_student')
 ]
