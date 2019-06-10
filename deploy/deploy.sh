c71b18a613fda34719fa7b1da69805b360e0ff82
matheusbsilva <matheusbattista@hotmail.com>
2019-06-10 16:04:47 -0300

    feat(#16): setup secret env vars to ci/cd

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,14 +2,14 @@
 #!/bin/bash
 
 echo "Setting up GCLOUD auth\n"
-gcloud auth activate-service-account --key-file ${TRAVIS_BUILD_DIR}/deploy/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${TRAVIS_BUILD_DIR}/deploy/gcloud-key.json
 
 gcloud --quiet config set project $PROJECT_ID
 gcloud --quiet config set container/cluster $CLUSTER 
 gcloud --quiet config set compute/zone $ZONE 
 
 echo "Getting cluster credentials\n"
-gcloud --quiet container clusters get-credentials $CLUSTER_STG
+gcloud --quiet container clusters get-credentials $CLUSTER
 
 echo "Authenticating on DockerHub\n"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
