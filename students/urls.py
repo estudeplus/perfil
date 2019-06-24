ddf87e27e55672650f3550916d4f37f346bf1eee
Victor Gomide <victoragomide@gmail.com>
2019-06-24 08:52:20 -0300

    Update urls.py
    Co-authored-by: Diego Resende <wdresende@gmail.com>

--- students/urls.py
+++ students/urls.py
@@ -3,8 +3,15 @@ from django.conf.urls import url
 from students import views
 
 urlpatterns = [
+    path('', views.StudentList.as_view(), name='list_students'),
+    path('new/', views.StudentCreate.as_view(), name='create_students'),
+    path('update/<int:id>/', views.StudentUpdate.as_view(), name='update_student'),
+    path('delete/<int:id>/', views.StudentDelete.as_view(), name='delete_student')
+
+    """
     path('', views.list_students, name='list_students'),
     path('new/', views.create_student, name='create_students'),
     path('update/<int:id>/', views.update_student, name='update_student'),
     path('delete/<int:id>/', views.delete_student, name='delete_student')
+    """
 ]
