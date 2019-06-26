b8ad1944cf2bd40a5c6d0500b2b2ca5d5f0559b6
Diego Resende <wdresende@gmail.com>
2019-06-26 01:54:10 -0300

    feat(#20): created route for update profile

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from students.views import StudentPreRegisterViewSet, StudentRegisterViewSet
+from students.views import StudentPreRegisterViewSet, StudentRegisterViewSet, StudentViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'pre-register', StudentPreRegisterViewSet)
-router.register(r'register', StudentRegisterViewSet)
+router.register(r'pre-register', StudentPreRegisterViewSet, basename='pre-register')
+router.register(r'register', StudentRegisterViewSet, basename='register')
+router.register(r'students', StudentViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
--- students/views.py
+++ students/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import UpdateModelMixin
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from .serializers import StudentPreRegisterSerializer, StudentRegisterSerializer
+from .serializers import StudentPreRegisterSerializer, StudentRegisterSerializer, StudentSerializer, StudentUpdateSerializer
 from django.urls import reverse_lazy
 
 from django.views.generic import ListView
@@ -29,6 +32,48 @@ class StudentRegisterViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status.HTTP_201_CREATED)
+
+class StudentViewSet(viewsets.ViewSet):
+
+    queryset = Student.objects.all()
+    serializer_class = StudentUpdateSerializer
+    
+    def list(self, request):
+        queryset = Student.objects.all()
+        serializer = StudentSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Student.objects.all()
+        student = get_object_or_404(queryset, pk=pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+    def update(self, request, pk=None):
+        queryset = Student.objects.all()
+        student = get_object_or_404(queryset, pk=pk)
+        serializer = StudentUpdateSerializer(student, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(student, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            student._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        queryset = Student.objects.all()
+        student = get_object_or_404(queryset, pk=pk)
+        self.perform_destroy(student)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class StudentForm(StudentForm):
     class Meta:
